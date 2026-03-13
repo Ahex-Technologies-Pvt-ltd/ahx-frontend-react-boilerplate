5cf248ac1cca1d0b6f7c0717972e85be329d51ea
Ch ShivaKumar <shivakumar.chervirala@ahex.co.in>
2026-03-13 15:32:16 +0530

    removed react-toastify

--- src/context/ToastProvider.tsx
+++ src/context/ToastProvider.tsx
@@ -1,28 +1,16 @@
 import type { ReactNode } from "react";
 import { Toaster } from "sonner";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-import { TOAST_LIBRARY } from "../toast.config";
-
 
 
 type Props = {
-  children: ReactNode
+    children: ReactNode
 }
 
 export default function ToastProvider({ children }: Props) {
     return (
         <>
             {children}
-
-            {TOAST_LIBRARY === "sonner" && (
-                <Toaster position="top-right" richColors={false} />
-            )}
-
-            {TOAST_LIBRARY === "toastify" && (
-                <ToastContainer position="top-right" />
-            )}
+            <Toaster position="top-right" richColors={false} />
         </>
     );
 }
